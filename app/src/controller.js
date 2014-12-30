18546925d4426ed24912bef90c021dc746473438
Carlo Colombo <carlo.colombo@gmx.com>
2014-12-30 19:39:01 +0100

    added logical deletion

--- app/src/controller.js
+++ app/src/controller.js
@@ -1,123 +1,143 @@
 angular.module('CashSplitter.controller', [])
-  .controller('TripController', function($scope, trip) {
-    $scope.trip = trip
-  })
-  .controller('TripListController', function($scope, trips) {
-    $scope.trips = trips;
-  })
-  .controller('TripNewController', function($scope, TripService, $state) {
-    $scope.trip = {
-      bills: [],
-      payments: []
-    };
-    $scope.submit = function() {
-      TripService.add($scope.trip).then(function() {
-        $state.go('trip.show', {
-          trip_id: $scope.trip.name
-        });
-      });
-    };
-  })
-  .controller('TripShowController', function($scope, TripService, $state, entries) {
-    TripService.totals($scope.trip._id).then(function(data) {
-      $scope.splitted = data
+    .controller('TripController', function($scope, trip) {
+        $scope.trip = trip
     })
-
-    $scope.entries = entries
-    $scope.remove = function(trip) {
-      if (confirm("Do you want to delete this trip?")) {
-        TripService.remove(trip).then(function() {
-          $state.go('trip_list')
+    .controller('TripListController', function($scope, trips) {
+        $scope.trips = trips;
+    })
+    .controller('TripNewController', function($scope, TripService, $state) {
+        $scope.trip = {
+            bills: [],
+            payments: []
+        };
+        $scope.submit = function() {
+            TripService.add($scope.trip).then(function() {
+                $state.go('trip.show', {
+                    trip_id: $scope.trip.name
+                });
+            });
+        };
+    })
+    .controller('TripShowController', function($scope, TripService, $state, entries) {
+        TripService.totals($scope.trip._id).then(function(data) {
+            $scope.splitted = data
         })
-      }
-    }
-    $scope.clean = function(trip) {
 
-      if (confirm("Do you want to delete all bills of this trip?")) {
-        trip.bills = []
-        trip.payments = []
-
-        TripService.add(trip).then(function() {
-          $state.go('trip.show', null, {
-            reload: true
-          })
-        })
-      }
-    }
-  })
-  .controller('BillNewController', function($scope, TripService, $state) {
-    $scope.bill = {
-      creationDate: new Date(),
-      _id: PouchDB.utils.uuid(),
-      splitters: $scope.trip.splitters.slice(0)
-    }
-
-    $scope.submit = function() {
-      $scope.bill.amount = $scope.bill.amount
-      $scope.trip.bills.push($scope.bill)
-
-      TripService.add($scope.trip).then(function() {
-        $state.go('trip.show', null, {
-          reload: true
-        })
-      })
-    }
-
-  })
-  .controller('FairBillNewController', function($scope, TripService, $state) {
-    $scope.bill = {}
-    $scope.creationDate = new Date()
-    $scope.sum = function(bill) {
-      return _.reduce(bill, function(tot, val) {
-        return parseFloat(tot) + parseFloat(val)
-      })
-    }
-    $scope.submit = function(bill, payer) {
-      _.each(
-        _.map(
-          _.pick(bill,function(amount){return !!amount}), function(amount, splitter) {
-        return {
-          creationDate: $scope.creationDate,
-          _id:PouchDB.utils.uuid(),
-          splitters : [splitter],
-          amount: amount,
-          payer: payer,
-          description: $scope.description
+        $scope.entries = entries
+        $scope.remove = function(trip) {
+            if (confirm("Do you want to delete this trip?")) {
+                TripService.remove(trip).then(function() {
+                    $state.go('trip_list')
+                })
+            }
         }
-      }),function (bill) {
-        $scope.trip.bills.push(bill)
-      })
+        $scope.clean = function(trip) {
 
-      TripService.add($scope.trip).then(function() {
-        $state.go('trip.show', null, {
-          reload: true
-        })
-      })
-    }
-  })
-  .controller('PaymentNewController', function($scope, TripService, $state) {
-    $scope.payment = {
-      creationDate: new Date(),
-      _id: PouchDB.utils.uuid()
-    }
+            if (confirm("Do you want to delete all bills of this trip?")) {
+                trip.bills = []
+                trip.payments = []
 
-    $scope.submit = function() {
-      $scope.trip.payments.push($scope.payment)
-      TripService.add($scope.trip).then(function() {
-        $state.go('trip.show', null, {
-          reload: true
-        })
-      })
-    }
+                TripService.add(trip).then(function() {
+                    $state.go('trip.show', null, {
+                        reload: true
+                    })
+                })
+            }
+        }
 
-  })
-  .controller('SplitterController', function($scope, $stateParams) {
-    $scope.splitter = $stateParams.splitter
-  })
-  .controller('SplitterShowController', function($scope, entries) {
-    $scope.entries = entries
-  })
-  .controller('EntryController', function($scope, entry) {
-    $scope.entry = entry
-  })
-  .controller('EntryShowController', angular.noop)
+        $scope.delete = function(id) {
+            if (confirm("Do you want to delete this transaction?")) {
+                (_.findWhere($scope.trip.bills, {
+                    _id: id
+                }) || {}).__deleted = true;
+                (_.findWhere($scope.trip.payments, {
+                    _id: id
+                }) || {}).__deleted = true;
+                TripService.add($scope.trip).then(function() {
+                    $state.go('trip.show', null, {
+                        reload: true
+                    })
+                })
+            }
+        }
+    })
+    .controller('BillNewController', function($scope, TripService, $state) {
+        $scope.bill = {
+            creationDate: new Date(),
+            _id: PouchDB.utils.uuid(),
+            splitters: $scope.trip.splitters.slice(0)
+        }
+
+        $scope.submit = function() {
+            $scope.bill.amount = $scope.bill.amount
+            $scope.trip.bills.push($scope.bill)
+
+            TripService.add($scope.trip).then(function() {
+                $state.go('trip.show', null, {
+                    reload: true
+                })
+            })
+        }
+
+    })
+    .controller('FairBillNewController', function($scope, TripService, $state) {
+        $scope.bill = {}
+        $scope.creationDate = new Date()
+        $scope.sum = function(bill) {
+            return _.reduce(bill, function(tot, val) {
+                return parseFloat(tot) + parseFloat(val)
+            })
+        }
+        $scope.submit = function(bill, payer) {
+            _.each(
+                _.map(
+                    _.pick(bill, function(amount) {
+                        return !!amount
+                    }),
+                    function(amount, splitter) {
+                        return {
+                            creationDate: $scope.creationDate,
+                            _id: PouchDB.utils.uuid(),
+                            splitters: [splitter],
+                            amount: amount,
+                            payer: payer,
+                            description: $scope.description
+                        }
+                    }),
+                function(bill) {
+                    $scope.trip.bills.push(bill)
+                })
+
+            TripService.add($scope.trip).then(function() {
+                $state.go('trip.show', null, {
+                    reload: true
+                })
+            })
+        }
+    })
+    .controller('PaymentNewController', function($scope, TripService, $state) {
+        $scope.payment = {
+            creationDate: new Date(),
+            _id: PouchDB.utils.uuid()
+        }
+
+        $scope.submit = function() {
+            $scope.trip.payments.push($scope.payment)
+            TripService.add($scope.trip).then(function() {
+                $state.go('trip.show', null, {
+                    reload: true
+                })
+            })
+        }
+
+    })
+    .controller('SplitterController', function($scope, $stateParams) {
+        $scope.splitter = $stateParams.splitter
+    })
+    .controller('SplitterShowController', function($scope, entries) {
+        $scope.entries = entries
+    })
+    .controller('EntryController', function($scope, entry) {
+        $scope.entry = entry
+    })
+    .controller('EntryShowController', angular.noop)
--- app/src/views.js
+++ app/src/views.js
@@ -1,45 +1,46 @@
 angular.module('CashSplitter.views', [])
-  .constant('views', {
-    double_entry: {
-      map: (function(trip) {
-        trip.bills.forEach(function(bill) {
-          var s = bill.splitters.length;
-          bill.splitters.forEach(function(splitter) {
-            emit([trip._id, splitter], parseFloat(bill.amount / s));
-          });
-          emit([trip._id, bill.payer], -parseFloat(bill.amount));
-        });
-        trip.payments.forEach(function(payment) {
-          emit([trip._id, payment.source], -parseFloat(payment.amount));
-          emit([trip._id, payment.target], parseFloat(payment.amount));
-        });
-      }).toString(),
-      reduce: '_sum'
-    },
-    trip_entries: {
-      map: (function(trip) {
-        trip.bills.forEach(function(bill) {
-          bill.type = "bill"
-          emit([trip._id, bill.creationDate.toJSON(), bill.payer], bill)
-        })
-        trip.payments.forEach(function(payment) {
-          payment.type = "payment"
-          payment.description = "-> " + payment.target
-          emit([trip._id, payment.creationDate.toJSON(), payment.source], payment)
-        })
-      }).toString()
-    },
-    entries: {
-      map: (function(trip) {
-        trip.bills.forEach(function(bill) {
-          bill.type = "bill"
-          emit(bill._id, bill)
-        })
-        trip.payments.forEach(function(payment) {
-          payment.type = "payment"
-          payment.description = "-> " + payment.target
-          emit(payment._id, payment)
-        })
-      }).toString()
-    }
-  })
+    .constant('views', {
+        double_entry: {
+            map: (function(trip) {
+                trip.bills.forEach(function(bill) {
+                    var s = bill.splitters.length;
+                    if (bill.__deleted) return
+                    bill.splitters.forEach(function(splitter) {
+                        emit([trip._id, splitter], parseFloat(bill.amount / s));
+                    });
+                    emit([trip._id, bill.payer], -parseFloat(bill.amount));
+                });
+                trip.payments.forEach(function(payment) {
+                    !payment.__deleted && emit([trip._id, payment.source], -parseFloat(payment.amount));
+                    !payment.__deleted && emit([trip._id, payment.target], parseFloat(payment.amount));
+                });
+            }).toString(),
+            reduce: '_sum'
+        },
+        trip_entries: {
+            map: (function(trip) {
+                trip.bills.forEach(function(bill) {
+                    bill.type = "bill";
+                    !bill.__deleted && emit([trip._id, bill.creationDate.toJSON(), bill.payer], bill);
+                })
+                trip.payments.forEach(function(payment) {
+                    payment.type = "payment"
+                    payment.description = "-> " + payment.target;
+                    !payment.__deleted && emit([trip._id, payment.creationDate.toJSON(), payment.source], payment)
+                })
+            }).toString()
+        },
+        entries: {
+            map: (function(trip) {
+                trip.bills.forEach(function(bill) {
+                    bill.type = "bill";
+                    !bill.__deleted && emit(bill._id, bill)
+                })
+                trip.payments.forEach(function(payment) {
+                    payment.type = "payment"
+                    payment.description = "-> " + payment.target;
+                    !payment.__deleted && emit(payment._id, payment)
+                })
+            }).toString()
+        }
+    })
